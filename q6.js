a15a84f24da26781f1a7f153b1f0566efc09b370
Amitai Levi <amitailevi86@gmail.com>
2021-01-19 19:42:12 +0200

    q6

---
+++ q6.js
@@ -0,0 +1,26 @@
+function run() {
+  const input = document.getElementById("input").value;
+  const output = document.getElementById("output");
+  let out = "";
+  /**/
+  dashes(input);
+  function dashes(input) {
+    let num = parseInt(input);
+
+    if (isNaN(num)) {
+      out = "The input is not a number";
+    } else {
+      for (let i = 0; i < input.length - 1; i++) {
+        out += input[i];
+
+        if (parseInt(input[i]) % 2 === 0 && parseInt(input[i + 1]) % 2 === 0) {
+          out += "-";
+        }
+      }
+      out += input[input.length - 1];
+    }
+  }
+
+  /**/
+  output.innerText = out;
+}
