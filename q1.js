493a555e7dccb3bda1060a4db6880384e453a310
Amitai Levi <amitailevi86@gmail.com>
2021-01-19 11:45:17 +0200

    q1 notes

--- q1.js
+++ q1.js
@@ -4,6 +4,9 @@ function run() {
   let out = "";
   /**/
   is_array(input);
+  // since in this case input wiil always be a string, because the input type
+  // is text (at least I don't know a way), I reffer to it as true if it is
+  // written like an array - inside [].
   function is_array(input) {
     if (input.indexOf("[") === 0 && input.indexOf("]") === input.length - 1) {
       out = true;
@@ -11,6 +14,8 @@ function run() {
       out = false;
     }
   }
+  // if there is an option to insert a real array, this function will check it
+  // more accurately
   function is_array1(input) {
     out = Array.isArray(input);
   }
