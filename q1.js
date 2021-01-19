a9b7c3f83cfa2a992fdf0508088926a3a170151f
Amitai Levi <amitailevi86@gmail.com>
2021-01-19 11:44:51 +0200

    q1

--- q1.js
+++ q1.js
@@ -1,11 +1,19 @@
 function run() {
-    const input = document.getElementById("input").value;
-    const output = document.getElementById("output");
-    let out = "";
-/**/
-    // write your code here
-    // out = input + 5;
-
-/**/
-    output.innerText = out;
+  const input = document.getElementById("input").value;
+  const output = document.getElementById("output");
+  let out = "";
+  /**/
+  is_array(input);
+  function is_array(input) {
+    if (input.indexOf("[") === 0 && input.indexOf("]") === input.length - 1) {
+      out = true;
+    } else {
+      out = false;
+    }
+  }
+  function is_array1(input) {
+    out = Array.isArray(input);
+  }
+  /**/
+  output.innerText = out;
 }
